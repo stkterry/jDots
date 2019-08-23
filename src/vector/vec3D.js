2f24c785be401ba1db1363294b4b665d53ecd804
Steven K. Terry <stkterry@gmail.com>
2019-08-22 22:59:57 -0400

    Push small changes, added vec3D, added small touchup here and there

--- src/vector/vec3D.js
+++ src/vector/vec3D.js
@@ -1,39 +1,45 @@
 const TWO_PI = Math.PI;
 
-class Vec2D {
-  constructor(x = 0, y = 0) {
+class Vec3D {
+  constructor(x = 0, y = 0, z = 0) {
     this.x = x;
     this.y = y;
+    this.z = z;
   }
 
   // Common operations
 
-  set(x, y) {
+  set(x, y, z) {
     this.x = x;
     this.y = y;
+    this.z = z;
   }
 
   add(vec) {
     this.x += vec.x;
     this.y += vec.y;
+    this.z += vec.z;
     return this;
   }
 
   sub(vec) {
     this.x -= vec.x;
     this.y -= vec.y;
+    this.z -= vec.z;
     return this;
   }
 
   scale(scalar) {
     this.x *= scalar;
     this.y *= scalar;
+    this.z *= scalar;
     return this;
   }
 
   div(mag) {
     this.x /= mag;
     this.y /= mag;
+    this.z /= mag;
     return this;
   }
 
@@ -44,29 +50,33 @@ class Vec2D {
 
   // get operations
   getNorm() {
-    return Math.sqrt( this.x * this.x + this.y * this.y);
+    return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
   }
 
   getHeading() {
-    return Math.atan2(this.y, this.x);
+    let rho = this.getNorm();
+    let phi = acos(this.z, rho);
+    let theta = atan2(this.y, this.x);
+    return {rho, phi, theta};
   }
 
   // helpers
 
   dup() {
-    return new Vec2D(this.x, this.y);
+    return new Vec3D(this.x, this.y, this.z);
   }
 
   dupUnit() {
     let norm = this.getNorm();
-    return new Vec2D(this.x / norm, this.y / norm);
+    return new Vec2D(this.x / norm, this.y / norm, this.z / norm);
   }
 
   distTo(vec) {
     let xDiff = this.x - vec.x;
     let yDiff = this.y - vec.y;
+    let zDiff = this.z = vec.z;
     return Math.sqrt(
-      xDiff * xDiff + yDiff * yDiff
+      xDiff * xDiff + yDiff * yDiff + zDiff * zDiff
     );
   }
 
@@ -84,24 +94,20 @@ class Vec2D {
   // Class methods
 
   static dot(vec1, vec2) {
-    return (vec1.x * vec2.x + vec1.y * vec2.y)
+    return (vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z)
   }
 
   static cross(vec1, vec2) {
-    return (vec1.x * vec2.y - vec1.y * vec2.x)
+    let x = vec1.y * vec2.z - vec1.z * vec2.y;
+    let y = vec1.z * vec2.x - vec1.x * vec2.z;
+    let z = vec1.x * vec2.y - vec1.y * vec2.x;
+    return new Vec3D(x, y, z);
   }
 
   static angleBetween(vec1, vec2) {
     return Math.acos(
       Vec2D.dot(vec1, vec2) / (vec1.getNorm() * vec2.getNorm())
     )
-  }
-
-  static angleRel(vec1, vec2) {
-    let angle = Math.atan2(vec2.y, vec2.x) - Math.atan2(vec1.y, vec1.x);
-    if (angle > Math.PI) angle -= TWO_PI
-    else if (angle < -Math.PI) angle += TWO_PI;
-    return angle; 
   }
 
   // // Class Methods gen
@@ -111,13 +117,15 @@ class Vec2D {
       minMag = 0;
     }
     const mag = (maxMag - minMag) * random() + minMag,
-      angle = random() * Math.TWO_PI,
-      x = mag * cos(angle),
-      y = mag * sin(angle);
-
-    return new Vec2D(x, y);
+    const theta = Math.random() * Math.PI;
+    const phi = Math.random() * Math.PI * 2 - Math.PI;
+    return new Vec3D(
+      mag * Math.sin(theta) * Math.cos(phi),
+      mag * Math.sin(theta) * Math.sin(phi),
+      mag * Math.cos(theta)
+      );
   };
 
 }
 
-export default Vec2D;
+export default Vec3D;
